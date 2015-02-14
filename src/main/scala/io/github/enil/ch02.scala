0f80e81a33fa4949be2f27110152364ec0d95291
Emil Nilsson <emil.nilsson@tactel.se>
2015-02-14 16:21:43 +0100

    Add solution for exercise 2.2

--- src/main/scala/io/github/enil/ch02.scala
+++ src/main/scala/io/github/enil/ch02.scala
@@ -25,13 +25,55 @@ object Exercise21 {
    */
   def fib(n: Int): Int = {
     def go(c: Int, n: Int, left: Int, right: Int): (Int, Int) =
-      if (c < n)
+      if (c < n) {
         // calculate the next number
         go(c + 1, n, right, left + right)
-      else
+      } else {
         // at number n
         (left, right)
+      }
 
     go(0, n, 0, 1)._1
+  }
+}
+
+/**
+ * Exercise 2.2: implement isSorted
+ *
+ * @author Emil Nilsson
+ */
+object Exercise22 {
+  def main(args: Array[String]): Unit = {
+    val ltInt = (a: Int, b: Int) => a < b
+    assert(isSorted(Array(1, 2, 3), ltInt) == true)
+    assert(isSorted(Array(1, 1, 3), ltInt) == false)
+    assert(isSorted(Array(1, 2, 3, 2), ltInt) == false)
+
+    val gtInt = (a: Int, b: Int) => a > b
+    assert(isSorted(Array(3, 2, 1), gtInt) == true)
+    assert(isSorted(Array(3, 2, 2), gtInt) == false)
+    assert(isSorted(Array(3, 2, 1, 2), gtInt) == false)
+
+    val ltString = (a: String, b: String) => a < b
+    assert(isSorted(Array("bar", "baz", "foo"), ltString) == true)
+    assert(isSorted(Array("bar", "baz", "baz"), ltString) == false)
+    assert(isSorted(Array("bar", "baz", "foo", "baz"), ltString) == false)
+  }
+
+  /**
+   * Checks whether an array is sorted according to a comparison function.
+   *
+   * @param as an array
+   * @param ordered a comparison function
+   * @tparam A the element type of the array
+   * @return true if the array is sorted according to the function
+   */
+  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+    def go(n: Int, o: Boolean): Boolean =
+      if (!o) false
+      else if (as.length - n > 1) go(n + 1, ordered(as(n), as(n + 1)))
+      else true
+
+    go(0, true)
   }
 }
