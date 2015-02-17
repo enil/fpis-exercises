7bef925d29a9f4fd0feca9ef4b17a82eb7eeac68
Emil Nilsson <emil.nilsson@tactel.se>
2015-02-17 20:04:24 +0100

    Copy the function foldRight into the List object

--- src/main/scala/io/github/enil/ch03.scala
+++ src/main/scala/io/github/enil/ch03.scala
@@ -43,6 +43,11 @@ with Exercise36.ListExtension {
   def apply[A](as: A*): List[A] =
     if (as.isEmpty) Nil
     else Cons(as.head, apply(as.tail: _*))
+
+  def foldRight[A,B](as: List[A],z: B)(f: (A, B) => B): B = as match {
+    case Nil => z
+    case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+  }
 }
 
 /**
