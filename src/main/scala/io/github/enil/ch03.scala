a0f0a248003bcbb4fd05eb76b8d8d698772728c4
Emil Nilsson <emil.nilsson@tactel.se>
2015-02-21 12:12:14 +0100

    Add the Tree trait and its subclasses
    * Update the copyright information in ch03.scala.
    * Add the code for the trait Tree and its subclasses Leaf and Branch.

--- src/main/scala/io/github/enil/ch03.scala
+++ src/main/scala/io/github/enil/ch03.scala
@@ -1,7 +1,8 @@
-package io.github.enil
-
 /**
- * The trait List, its subtraits and its companion object List is copied verbatim from Functional Programming in Scala.
+ * Some of the traits, objects and classes are based on the source code from Functional Programming in Scala.
+ * The original source code from Functional Programming in Scala can be found at https://github.com/fpinscala/fpinscala.
+ * These code sections are copyrighted by Manning Publications, Co:
+ *
  *
  * Copyright (c) 2012, Manning Publications, Co.
  *
@@ -17,13 +18,54 @@ package io.github.enil
  * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
  * OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
  * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ *
+ * All other code sections are copyrighted by Emil Nilsson:
+ *
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Emil Nilsson
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package io.github.enil
+
+/**
+ * From Functional Programming in Scala.
  */
 sealed trait List[+A]
 
+/**
+ * From Functional Programming in Scala.
+ */
 case object Nil extends List[Nothing]
 
+/**
+ * From Functional Programming in Scala.
+ */
 case class Cons[+A](head: A, tail: List[A]) extends List[A]
 
+/**
+ * From Functional Programming in Scala.
+ */
 object List extends Exercise32.ListExtension
 with Exercise33.ListExtension
 with Exercise34.ListExtension
@@ -63,6 +105,21 @@ with Exercise324.ListExtension {
     case Cons(x, xs) => f(x, foldRight(xs, z)(f))
   }
 }
+
+/**
+ * From Functional Programming in Scala.
+ */
+sealed trait Tree[+A]
+
+/**
+ * From Functional Programming in Scala.
+ */
+case class Leaf[A](value: A) extends Tree[A]
+
+/**
+ * From Functional Programming in Scala.
+ */
+case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 /**
  * Exercise 3.2: implement List.tail.
