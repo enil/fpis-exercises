722a074af586920eb6f9789787223b87def670de
Emil Nilsson <emil.nilsson@tactel.se>
2015-02-25 15:26:54 +0100

    Add trait Stream and its implementations to ch05
    * Create the source code file ch05.scala.
    * Add the trait Stream and its implementations Cons and Empty and its
    companion object Stream from the code from Functional Programming in
    Scala.

---
+++ src/main/scala/io/github/enil/chapter05/ch05.scala
@@ -0,0 +1,80 @@
+/*
+ * Some of the traits, objects and classes are based on the source code from Functional Programming in Scala.
+ * The original source code from Functional Programming in Scala can be found at https://github.com/fpinscala/fpinscala.
+ * These code sections are copyrighted by Manning Publications, Co:
+ *
+ *
+ * Copyright (c) 2012, Manning Publications, Co.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+ * OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ *
+ * All other code sections are copyrighted by Emil Nilsson:
+ *
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Emil Nilsson
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package io.github.enil.chapter05
+
+/**
+ * From Functional Programming in Scala.
+ */
+sealed trait Stream[+A]
+
+/**
+ * From Functional Programming in Scala.
+ */
+case object Empty extends Stream[Nothing]
+
+/**
+ * From Functional Programming in Scala.
+ */
+case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
+
+/**
+ * From Functional Programming in Scala.
+ */
+object Stream {
+  def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
+    lazy val head = hd
+    lazy val tail = tl
+    Cons(() => head, () => tail)
+  }
+
+  def empty[A]: Stream[A] = Empty
+
+  def apply[A](as: A*): Stream[A] =
+    if (as.isEmpty) empty else cons(as.head, apply(as.tail: _*))
+}
